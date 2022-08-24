499a94da7ec035b4c780c7c330e4eca093006dba
naomiduan <108888083+naomiduan@users.noreply.github.com>
2022-08-23 22:27:20 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,35 +1,40 @@
 
-const questionObject = {
-  question: 'What is my question?',
-  answer: 'This is my answer',
-  displayQuestion: function () {
-    alert( this.question );
-  },
-  displayAnswer: function () {
-    alert( this.answer );
-  }
-}
 
-
-class question {
-  // A constructor is a special function used to create an instance of the class
+class questionclass {
   constructor(question, answer) {
     this.question = question;
     this.answer = answer;
-    // We will set this to true when answered correctly.
-    this.correct = false;
   }
 }
 
-
 let myFlashcards = [];
+let count = 0;
 
-myFlashcards.push(new question('What is 3+5?', '8'));
-myFlashcards.push(new question('What color is the sky?', 'blue'));
-myFlashcards.push(new question('Is ice cold?', 'yes'));
+myFlashcards.push(new questionclass("What is 3+5?", "8"));
+myFlashcards.push(new questionclass("What color is the sky?", "blue"));
+myFlashcards.push(new questionclass("Is ice cold?", "yes"));
 
+setup_display_event();
 
-for (let i = 0; i < myFlashcards.length; i++){
-  console.log(myFlashcards[i].question);
-  console.log(myFlashcards[i].answer);
+document.getElementById("question").innerHTML = myFlashcards[0].question;
+
+function setup_display_event() {
+  console.log("Please click question to move to the next event.");
+  document.getElementById("question").onclick = function () {
+    
+    count++;
+
+    // Get integer part, for example 1 / 2 = 0
+    let question_index = Math.floor(count / 2);
+    if (question_index >= myFlashcards.length) return;
+
+    if (count % 2 === 1) {
+      let ele = document.getElementById("answer");
+      ele.innerHTML = myFlashcards[question_index].answer;
+    } else {
+      document.getElementById("answer").innerHTML = " ";
+      document.getElementById("question").innerHTML =
+        myFlashcards[question_index].question;
+    }
+  };
 }

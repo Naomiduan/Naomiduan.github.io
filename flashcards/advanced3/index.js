9222c8041c3d94e343f3dec1a477e83d0b3fe7fb
naomiduan <108888083+naomiduan@users.noreply.github.com>
2022-08-24 21:34:38 -0400

    Update index.js

--- flashcards/advanced3/index.js
+++ flashcards/advanced3/index.js
@@ -1,55 +1,69 @@
 //create a question class
 class questionclass {
-  constructor(question, answer,pic) {
+  constructor(question, answer) {
     this.question = question;
     this.answer = answer;
-    this.pic = pic;
+
   }
 }
 
 let myFlashcards = [];
 let count = 0;
-
-var img1 = document.createElement("img1");
-img1.src = "math.png";
-var img2 = document.createElement("img2");
-img2.src = "sky.png";
-var img3 = document.createElement("img3");
-img3.src = "ice.png";
-
-
+document.getElementById("img1").style.display = "none";
+document.getElementById("img2").style.display = "none";
+document.getElementById("img3").style.display = "none";
 //create question objects
-myFlashcards.push(new questionclass("What is 3+5?", "8", img1));
-myFlashcards.push(new questionclass("What color is the sky?", "blue", img2));
-myFlashcards.push(new questionclass("Is ice cold?", "yes", img3));
+myFlashcards.push(new questionclass("What is 3+5?", "8"));
+myFlashcards.push(new questionclass("What color is the sky?", "blue"));
+myFlashcards.push(new questionclass("Is ice cold?", "yes"));
 
-setup_display_event();
-// Intialize the question for HTML
-document.getElementById("question").innerHTML = myFlashcards[0].question;
+console.log("Please hover question to see answer&pic/click to move to another event.");
 
-// This function is to count the number of clicking events, and dislay answer and question intermittently
-function setup_display_event() {
-  console.log("Please hover question to see answer/move to another event.");
+setElementbyID();
+
+  function setElementbyID(){
   document.getElementById("question").onmouseover = function () {
     
     count++;
 
     // Get integer part, for example 1 / 2 = 0
-    let question_index = Math.floor(count / 2);
-    if (question_index >= myFlashcards.length) return;
 
-    if (count % 2 === 1) {
+    if (count === 1) {
+      let question_index = count - 1;
       //display the answer of the current question
+      document.getElementById("img1").style.display = "inline";
       let ele = document.getElementById("answer");
       ele.innerHTML = myFlashcards[question_index].answer;
-      let img = document.getElementById("img1");
-      img.innerHTML = img1.src;
+      ;
+    }  else if (count === 2){
+      let question_index = count - 1;
+      let ele = document.getElementById("answer");
+      ele.innerHTML = myFlashcards[question_index].answer;
+      document.getElementById("question").innerHTML = myFlashcards[question_index].question;
+      document.getElementById("answer").style.display = "none";
+      document.getElementById("img1").style.display = "none";
+
+    }else if (count === 3){
+      let question_index = count - 2;
+      document.getElementById("answer").style.display = "inline";
+      document.getElementById("answer").innerHTML = myFlashcards[question_index].answer;
+      document.getElementById("img2").style.display = "inline";
       
-    } else {
-      //display next question
-      document.getElementById("answer").innerHTML = " ";
-      document.getElementById("question").innerHTML =
-        myFlashcards[question_index].question;
+    }else if(count === 4){
+      let question_index = count - 2;
+      document.getElementById("question").innerHTML = myFlashcards[question_index].question;
+      document.getElementById("img2").style.display = "none";
+      document.getElementById("answer").style.display = "none";
+    }else if(count === 5){
+      let question_index = count - 3;
+      document.getElementById("answer").innerHTML = myFlashcards[question_index].answer;
+      document.getElementById("answer").style.display = "inline";
+      document.getElementById("img3").style.display = "inline";
     }
-  };
+  }
+
 }
+
+
+
+
